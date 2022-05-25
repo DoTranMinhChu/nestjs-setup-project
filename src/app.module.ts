ab5ed8fc420790f201fea4610a0158d609d34dc4
chudtmse150303 <chudtmse150303@fpt.edu.vn>
2022-05-25 22:37:20 +0700

    setup nestjs + authentication + authorization

---
+++ src/app.module.ts
@@ -0,0 +1,59 @@
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppService } from '@app/app.service';
+import configuration from '@config/configuration';
+import { AuthModule } from '@service/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from '@guard/auth.guard';
+import { RolesGuard } from '@guard/role.guard';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfig, DB_PATH_CONFIG } from '@config/database.config';
+import { UserModule } from '@service/user/user.module';
+import { AuthController } from '@api/auth.controller';
+import { UserController } from '@api/user.controller';
+import { UtilController } from '@api/util.controller';
+import { UtilModule } from '@service/util/util.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    CacheModule.register({
+      ttl: 0,
+      isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return configService.get<DatabaseConfig>(DB_PATH_CONFIG);
+      },
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UserModule,
+    UtilModule,
+  ],
+  controllers: [
+    AuthController,
+    UserController,
+    UtilController,
+  ],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
+})
+export class AppModule { }
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from '@app/app.module';
+import { ServerConfig } from '@config/server.config';
+import { SwaggerConfig } from '@config/swagger.config';
+import { Environment } from '@constant/environment.enum';
+import { AllExceptionsFilter } from '@filter/all-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+  const serverConfig = config.get<ServerConfig>('server');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  if (serverConfig.env == Environment.Development) {
+    const document = SwaggerModule.createDocument(
+      app,
+      SwaggerConfig.getDocumentConfig(),
+    );
+    SwaggerModule.setup('api/document', app, document);
+  }
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.enableCors();
+  await app.listen(serverConfig.port);
+}
+
+bootstrap();
